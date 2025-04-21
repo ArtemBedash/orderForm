af0c845e13d955427b1e22d675540662147eb13c
ArtemBedash <artembedash@TimonChik.local>
2025-04-21 23:26:20 +0300

    Fix base path configuration for GitHub Pages

--- src/components/pages/ConfirmationPage.tsx
+++ src/components/pages/ConfirmationPage.tsx
@@ -1,11 +1,16 @@
+import {useNavigate} from "react-router-dom";
 
 const ConfirmationPage = () => {
+    const navigate = useNavigate();
 
+    const handleClick = () => {
+        navigate('/');}
 
     return (
         <div>
 
             <h1>Авторизация прошла успешно</h1>
+            <button type='button' onClick={handleClick}>На главную</button>
 
         </div>
     );
