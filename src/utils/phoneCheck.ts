b15e49039c7acf04578bd682bd6fa526c7dfc108
ArtemBedash <artembedash@TimonChik.local>
2025-04-25 13:06:15 +0300

    Update project: latest changes and fixes

--- src/utils/phoneCheck.ts
+++
@@ -1,7 +0,0 @@
-export const checkPhone = (phone: string): Promise<boolean> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(phone.startsWith('+7'));
-    }, 500);
-  });
-};
